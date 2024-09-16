6293b0a83acf34cac843698ef93b5e1f759575e4
rajsmhs <raj.michaels@gmail.com>
2024-09-16 10:40:58 +0530

    Update process.py

--- process.py
+++ process.py
@@ -50,8 +50,23 @@ def write_dataframe(df,target_bucket,prefix,source_table,file_format,partition_f
           .mode("append")
           .save("s3://" + target_bucket + "/" + prefix + "/")
           )
-        
-    
+
+
+def compare_schemas(source_table,filter_condition,column_list,spark_session):
+    source_schema = spark_session.sql(f"select {', '.join(column_list)} from {source_table} where {filter_condition}").schema
+    target_schema = spark_session.sql(f"select {', '.join(column_list)} from {target_table} where {filter_condition}").schema
+    diff_fields = []
+    if len(source_schema.fields) != len(target_schema.fields):
+        diff_fields.append("source_column_count:{0},target_column_count:{1}".format(len(source_schema.fields),len(target_schema.fields)))
+    for field1, field2 in zip(source_schema.fields, target_schema.fields):
+        schema_diff = ""
+        if field1.name != field2.name or field1.dataType != field2.dataType:
+            schema_diff = "{s_name}:{s_type} <> {t_name}:{t_type}".format(s_name=field1.name, s_type=field1.dataType, t_name=field2.name, t_type=field2.dataType)
+            diff_fields.append(schema_diff)
+    return "".join(diff_fields)
+
+
+
 
 def accuracy_summary_validation(source_df,target_df,df_missing_rec,spark_session):
     accuracy_summary = [
@@ -60,8 +75,8 @@ def accuracy_summary_validation(source_df,target_df,df_missing_rec,spark_session
             "target_count":target_df.count(),
             "data_mismatched_records":df_missing_rec.count(),
             "data_mismatch_source":df_missing_rec.filter(col("Data_Mismatch")=="Data Missing Values At Source").count(),
-            "data_mismatch_target":df_missing_rec.filter(col("Data_Mismatch")=="Data Missing Values At Target").count()
-        
+            "data_mismatch_target":df_missing_rec.filter(col("Data_Mismatch")=="Data Missing Values At Target").count(),
+            "column_difference":column_diff
         }
     
     ]
